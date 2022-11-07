ca1d5d70f6cf18e4bd98a370c7effeded3753b7e
ajsaule <andrejsaule8@gmail.com>
2022-11-07 16:07:54 +1100

    add: logger

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const Person = require('./person'); // module import is called common.js 
+// const Person = require('./person'); // module import is called common.js 
 
-// instantiating person object from the Class 
-const person1 = new Person('Jon Doe', 30)
-// calling the greeting method 
-person1.greeting()
+// // instantiating person object from the Class 
+// const person1 = new Person('Jon Doe', 30)
+// // calling the greeting method 
+// person1.greeting()
 
-// console.log(person)
+// // console.log(person)
+
+const Logger = require('./logger')
+
+const logger = new Logger()
+
+logger.on('message', (data) => console.log('Called Listener', data))
+
+logger.log('Hello World')
+logger.log('Hi')
+logger.log('Hey')
